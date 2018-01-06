1dd22131cae1a4137d8f81ba6b332390392b2dd0
sbhtkx <danielsaban2015@gmail.com>
2018-01-06 18:31:41 +0200

    stam

--- app/src/main/java/com/example/yaeli/smart_buy/MapsActivity.java
+++ app/src/main/java/com/example/yaeli/smart_buy/MapsActivity.java
@@ -11,7 +11,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-//SKFHDKLJSHF
     private GoogleMap mMap;
 
     @Override
